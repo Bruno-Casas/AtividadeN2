2ae9630c0e2645d44e2d63552dc121bc8fcf8121
Bruno Casas <brunocasas04@gmail.com>
2021-07-01 21:22:05 -0400

    Adiciona sql do banco

---
+++ resources/database.sql
@@ -0,0 +1,18 @@
+CREATE database desculpemas;
+
+CREATE TABLE usuario (
+    id serial PRIMARY KEY,
+	nome varchar(128) UNIQUE NOT NULL,
+	password varchar(32) NOT NULL,
+	email varchar(128) UNIQUE NOT NULL,
+	data_nascimento date NOT NULL
+);
+
+CREATE TABLE desculpa (
+    id serial PRIMARY KEY,
+	frase text UNIQUE NOT NULL,
+	usuario_id int NOT NULL,
+    CONSTRAINT fk_usuario_id
+        FOREIGN KEY(usuario_id) 
+            REFERENCES usuario(id)
+);
